cc0c9b526c37c9a05e63109787ee36ea192554e8
davydova-katerina <davydova.katerina84@gmail.com>
2025-10-02 15:24:51 +0300

    final_project

--- tests/test_ui.py
+++ tests/test_ui.py
@@ -1,7 +1,5 @@
 import pytest
 import allure
-import sys
-import os
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
