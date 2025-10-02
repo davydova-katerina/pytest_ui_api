ab71c7ec473f38a113b79b7ff0c03463f7e5bcc7
davydova-katerina <davydova.katerina84@gmail.com>
2025-10-02 16:54:10 +0300

    final_project

--- pages/search_page.py
+++ pages/search_page.py
@@ -5,28 +5,32 @@ import time
 
 
 class SearchPage(BasePage):
-    """Класс для работы со страницей поиска"""
+    """Класс для работы со страницей поиска."""
 
     # Обновленные локаторы
     SEARCH_RESULTS = (By.CSS_SELECTOR, "div.product.need-watch, div.product-card")
     BOOK_TITLE = (By.CSS_SELECTOR, "a.product-title-link, a.product-card__title")
-    ADD_TO_CART_BUTTON = (By.CSS_SELECTOR, "a.btn.buy-link.btn-primary, button.btn-buy")
-    CART_COUNTER = (By.CSS_SELECTOR, "span.b-header-b-personal-e-icon-count-m-cart, span.basket-in-cart-a")
-    NO_RESULTS_MESSAGE = (By.CSS_SELECTOR, "div.search-error, div.searchnoresults")
+    ADD_TO_CART_BUTTON = (By.CSS_SELECTOR,
+                          "a.btn.buy-link.btn-primary, button.btn-buy")
+    CART_COUNTER = (By.CSS_SELECTOR,
+                    "span.b-header-b-personal-e-icon-count-m-cart, "
+                    "span.basket-in-cart-a")
+    NO_RESULTS_MESSAGE = (By.CSS_SELECTOR,
+                          "div.search-error, div.searchnoresults")
     SEARCH_TITLE = (By.CSS_SELECTOR, "h1.searchtitle")
 
     @allure.step("Получить количество результатов поиска")
     def get_search_results_count(self):
-        """Получить количество найденных книг"""
+        """Получить количество найденных книг."""
         try:
             elements = self.driver.find_elements(*self.SEARCH_RESULTS)
             return len(elements)
-        except:
+        except Exception:
             return 0
 
     @allure.step("Получить заголовки найденных книг")
     def get_book_titles(self):
-        """Получить список заголовков найденных книг"""
+        """Получить список заголовков найденных книг."""
         titles = []
         try:
             elements = self.driver.find_elements(*self.BOOK_TITLE)
@@ -39,7 +43,7 @@ class SearchPage(BasePage):
 
     @allure.step("Добавить первую книгу в корзину")
     def add_first_book_to_cart(self):
-        """Добавить первую найденную книгу в корзину"""
+        """Добавить первую найденную книгу в корзину."""
         try:
             buttons = self.driver.find_elements(*self.ADD_TO_CART_BUTTON)
             if buttons:
@@ -52,27 +56,29 @@ class SearchPage(BasePage):
 
     @allure.step("Получить количество товаров в корзине")
     def get_cart_items_count(self):
-        """Получить количество товаров в корзине"""
+        """Получить количество товаров в корзине."""
         try:
             element = self.find_element(self.CART_COUNTER, timeout=5)
             return int(element.text) if element.text.isdigit() else 0
-        except:
+        except Exception:
             return 0
 
     @allure.step("Проверить наличие сообщения 'нет результатов'")
     def is_no_results_message_displayed(self):
-        """Проверить отображение сообщения об отсутствии результатов"""
+        """Проверить сообщение об отсутствии результатов."""
         return self.is_element_visible(self.NO_RESULTS_MESSAGE, timeout=5)
 
     @allure.step("Дождаться загрузки результатов поиска")
     def wait_for_search_results(self, timeout=10):
-        """Дождаться загрузки результатов поиска"""
+        """Дождаться загрузки результатов поиска."""
         try:
             self.wait.until(
-                lambda d: self.get_search_results_count() > 0 or
-                          self.is_no_results_message_displayed() or
-                          self.is_element_visible(self.SEARCH_TITLE, timeout=5)
+                lambda d: (
+                        self.get_search_results_count() > 0 or
+                        self.is_no_results_message_displayed() or
+                        self.is_element_visible(self.SEARCH_TITLE, timeout=5)
+                )
             )
             return True
-        except:
+        except Exception:
             return False
